aa4a499247b27462f4a12d3f78a0762bc1049cc5
Nukecraft5419 <72621737+Nukecraft5419@users.noreply.github.com>
2025-01-01 02:32:06 +0000

    feat: :sparkles: Adding @nuxt/content module in project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/google-fonts']
+  modules: ['@nuxtjs/google-fonts', '@nuxt/content']
 })
